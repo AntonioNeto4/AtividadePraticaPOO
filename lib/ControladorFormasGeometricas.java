90ef0ca5bc5a415b3aff14ba611ff41e4100d58e
AntonioPontes <AntonioPontes>
2025-04-15 11:03:59 -0300

    Adionando o começo da inteface

---
+++ lib/ControladorFormasGeometricas.java
@@ -0,0 +1,8 @@
+
+public class ControladorFormasGeometricas {
+        
+
+        // Precisamos verficar quais são os dados (raio, lados, altura) 
+        // Precisamos verificar quais são as formas e qual será o tipo de cálculo             
+    
+}
